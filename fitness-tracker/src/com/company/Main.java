cfdfe636b625b6ecb5b4a6d76d020555a6f6803e
kevin <khatymovrenat@gmail.com>
2020-04-13 23:15:34 +0300

    Реализация в отдельным классе. Добавлены профили

--- fitness-tracker/src/com/company/Main.java
+++ fitness-tracker/src/com/company/Main.java
@@ -11,101 +11,14 @@ package com.company;
  */
 
 import javax.naming.InvalidNameException;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
 
 public class Main {
 
-    public static boolean isInteger(Object object) {
-        if(object instanceof Integer) {
-            return true;
-        } else {
-            String string = object.toString();
-
-            try {
-                Integer.parseInt(string);
-            } catch(Exception e) {
-                return false;
-            }
-        }
-
-        return true;
+    public static void main(String[] args) throws IOException, InvalidNameException {
+        AppWorkout app = new AppWorkout ();
+        app.run();
     }
 
-    public static void main(String[] args) throws IOException, InvalidNameException  {
-        BufferedReader b_r = new BufferedReader(new InputStreamReader(System.in));
-        String cmd_op;
-
-        ArrayList<Training>  workout = new ArrayList<Training >();
-
-        workout.add(new Training( "push_ups", 345.0));
-        workout.add(new Training( "jump rope", 413.0));
-        workout.add(new Training( "squats", 284.1));
-
-        Profile dude = new Profile();
-
-        while (true) {
-            for (int i = 0; i < workout.size(); i++) {
-                System.out.print(i + 1 + " ");
-                workout.get(i).display();
-            }
-
-            System.out.println("Choose your workout:");
-            cmd_op = b_r.readLine();
-
-            if (!isInteger(cmd_op)) {
-                System.out.println("Write number and try again");
-                continue;
-            }
-
-            int train_num = Integer.parseInt(cmd_op);
-
-            if (train_num > workout.size() || train_num < 0) {
-                System.out.println("Wrong number, try again");
-                continue;
-            }
-
-            System.out.println("Start training?(y/n):");
-            cmd_op = b_r.readLine();
-
-            double start = 0, time = 0;
-            if (cmd_op.equals("y")) {
-                start = System.currentTimeMillis();
-                System.out.println("Write `stop` when you finish");
-                while (true) {
-                    if (cmd_op.equals("stop")) {
-                        time = (System.currentTimeMillis() - start) / (1e3 * 3600);
-                        workout.get(train_num - 1).all_cal += time * workout.get(train_num - 1).get_cal_hour();
-                        break;
-                    }
-                    cmd_op = b_r.readLine();
-                }
-            } else continue;
-
-            System.out.println("Time = " + time + " hour " + "\nyou spent " + workout.get(train_num - 1).all_cal + " cal\n" +
-                    "your " + workout.get(train_num - 1).get_type() + " was amazing, continue?(y/n)");
-
-            dude.all_cal += workout.get(train_num - 1).all_cal;
-            dude.all_time += time;
-
-            workout.get(train_num - 1).clear();
-
-            cmd_op = b_r.readLine();
-
-            if (cmd_op.equals("y")) {
-                continue;
-            } else if (cmd_op.equals("n")) {
-                System.out.println("\n\nWhole time = " + dude.all_time + " hour " + "\nyou spent " + dude.all_cal + " cal");
-                break;
-            } else System.out.println("Wrong, break");
-
-            break;
-
-        }
-
-    }
 }
 
--- fitness-tracker/src/com/company/Profile.java
+++ fitness-tracker/src/com/company/Profile.java
@@ -1,6 +1,32 @@
 package com.company;
 
 public class Profile {
-    public double all_cal = 0;
-    public double all_time = 0;
+    private String name;
+    private double all_cal = 0;
+    private double all_time = 0;
+
+    Profile(String name){
+        this.name = name;
+    }
+
+    public void add_time(double dt){
+        this.all_time += dt;
+    }
+
+    public void add_cal(double new_cal){
+        this.all_cal += new_cal;
+    }
+
+
+    public String get_name(){
+        return this.name;
+    }
+
+    public double get_time(){
+        return this.all_time;
+    }
+
+    public double get_cal(){
+        return this.all_cal;
+    }
 }
